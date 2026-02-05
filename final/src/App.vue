c9d9a5ff083e9f56d0dbac1dcab48f20b2e9bf36
Franco Acevedo <francoeacevedo37@gmail.com>
2026-02-05 16:45:34 -0300

    estructura basica con validaciones

--- final/src/App.vue
+++ final/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <LoginView/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginView from './components/LoginView.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginView
   }
 }
 </script>
