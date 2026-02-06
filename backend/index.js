45542c69cdab2bf0152086d4a51203ab2c32ea76
Franco Acevedo <francoeacevedo37@gmail.com>
2026-02-06 15:01:39 -0300

    backend ok, falta encontrar url que sirva para obtener los valores ya que da error al pedir los datos

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
-const port = 3001;
+const PORT = 3001;
 
-// midlewares sirven para agregar funcionalidades a las rutas, como por ejemplo, permitir
-//  el acceso a recursos desde otros dominios (CORS) o parsear el cuerpo de las solicitudes en formato JSON.
 app.use(cors());
 app.use(express.json());
 
-app.get('/price/:exchange/:crypto', async (req, res) => {
-    const [exchange, crypto] = req.params;
-    try {
-        const res = await axios.get(`https://criptoya.com/api/${exchange}/${crypto}/ARS/0.1`
-    );
-        res.json(res.data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching price data' });
-    }
+app.get("/price/:exchange/:crypto", async (req, res) => {
+  const { exchange, crypto } = req.params;
+
+  try {
+    const url = `https://criptoya.com/api/${exchange}/${crypto}/ars`;
+
+
+    const response = await axios.get(url);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("CriptoYa error:", error.response?.data || error.message);
+    res.status(500).json({ error: "Error fetching price data" });
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ Backend activo en http://localhost:${PORT}`);
 });
