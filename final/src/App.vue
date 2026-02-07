da41dc565015e1921197f29fb387f0dd6c78f77f
Franco Acevedo <francoeacevedo37@gmail.com>
2026-02-07 12:03:32 -0300

    creacion y modificacion de router y vistas

--- final/src/App.vue
+++ final/src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <div id="app">
-<LoginView @login-success="setUserID" />
-<DashboardView :userID="userID" />
-<CompraVenta />
+    <nav>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/market">Market</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import LoginView from './components/LoginView.vue';
-import DashboardView from './components/DashboardView.vue';
-import CompraVenta from './components/CompraVenta.vue';
-
-
 export default {
   name: 'App',
-  components: {
-    LoginView,
-    DashboardView,
-    CompraVenta,
-  },
-
-  data() {
-    return {
-      userID: ""
-    }
-  },
-  methods: {
-    setUserID(id) {
-      this.userID = id
-    }
-  }
 }
 </script>
 
